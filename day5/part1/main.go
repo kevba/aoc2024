275ed660c94be4843519da7a11c4a1143ce800b9
Kevin Batema <kevinbatema@gmail.com>
2024-12-05 12:30:25 +0100

    day 5; part 1 and part 2

---
+++ day5/part1/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"aoc2024"
+	"fmt"
+	"slices"
+	"strings"
+)
+
+func main() {
+	timer := aoc2024.Time()
+	defer timer()
+
+	input := aoc2024.GetInput()
+	answer := solve(input)
+	fmt.Println(answer)
+}
+
+func solve(lines []string) int {
+	count := 0
+	rules := [][]int{}
+	updates := [][]int{}
+
+	for i, l := range lines {
+		if l == "" {
+			for _, r := range lines[:i] {
+				rn := aoc2024.AtoiSlice(strings.Split(r, "|"))
+				rules = append(rules, rn)
+			}
+			for _, u := range lines[i+1:] {
+				un := aoc2024.AtoiSlice(strings.Split(u, ","))
+				updates = append(updates, un)
+			}
+			break
+		}
+	}
+
+	for _, pages := range updates {
+		if matchRule(rules, pages) {
+			middle := pages[(len(pages) / 2)]
+			count += middle
+		}
+	}
+
+	return count
+}
+
+func matchRule(rules [][]int, pages []int) bool {
+	for _, r := range rules {
+		if index := slices.Index(pages, r[0]); index > 0 {
+			indexBefore := slices.Index(pages[:index], r[1])
+			if indexBefore > -1 {
+				return false
+			}
+		}
+	}
+
+	return true
+
+}
